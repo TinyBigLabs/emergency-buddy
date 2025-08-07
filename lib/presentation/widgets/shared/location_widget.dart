c0e41b5c5471b8f2939102d77d6041fdf1d79651
Vera Austermann <mail@vera-austermann.de>
2025-08-07 09:10:48 +0100

    fix: web header and adding disclaimer

--- lib/presentation/widgets/shared/location_widget.dart
+++ lib/presentation/widgets/shared/location_widget.dart
@@ -1,6 +1,7 @@
 import 'package:emergency_buddy/core/location/gps_location.dart';
 import 'package:emergency_buddy/core/utils/constants.dart';
 import 'package:emergency_buddy/presentation/widgets/entry_icon_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LocationWidget extends StatefulWidget {
@@ -31,30 +32,39 @@ class _LocationWidgetState extends State<LocationWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: const EdgeInsets.symmetric(horizontal: UIConstants.mediumSize),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment:  MainAxisAlignment.start,
-        children: [
-          Text(
-            "Your Location is set to: \n$_location",
-            style: Theme.of(context).textTheme.headlineSmall,
-            maxLines: 1,
-            overflow: TextOverflow.visible,
-            softWrap: true,
-          ),
-          Text(
-            _location,
-            style: Theme.of(context).textTheme.bodyLarge,
-            maxLines: 3,
-            overflow: TextOverflow.visible,
-            softWrap: true,
-          ),
-          SizedBox(height: UIConstants.smallSize),
-        ],
-      ),
+    return kIsWeb? Row(
+      children: [
+        EntryIconWidget(icon: Icons.location_city),
+        SizedBox(width: UIConstants.smallSize),
+        Text(
+          "Your Location is set to: $_location" ,
+          style: kIsWeb? Theme.of(context).textTheme.bodyLarge : Theme.of(context).textTheme.headlineSmall,
+          maxLines: 1,
+          overflow: TextOverflow.visible,
+          softWrap: true,
+        ),
+        SizedBox(height: UIConstants.smallSize),
+      ],
+    ) : Column(
+      children: [
+        kIsWeb? EntryIconWidget(icon: Icons.language) : SizedBox.shrink(),
+        kIsWeb? SizedBox(width: UIConstants.smallSize) : SizedBox.shrink(),
+        Text(
+          kIsWeb? "Your Location is set to: $_location" : "Your Location is set to:" ,
+          style: kIsWeb? Theme.of(context).textTheme.bodyLarge : Theme.of(context).textTheme.headlineSmall,
+          maxLines: 1,
+          overflow: TextOverflow.visible,
+          softWrap: true,
+        ),
+        kIsWeb? SizedBox.shrink() : Text(
+          _location,
+          style: Theme.of(context).textTheme.bodyLarge,
+          maxLines: 3,
+          overflow: TextOverflow.visible,
+          softWrap: true,
+        ),
+        SizedBox(height: UIConstants.smallSize),
+      ],
     );
   }
 }
